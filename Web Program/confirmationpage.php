90e827eba38459b0e00f6e51f94133e939889eaa
Whadiyatalkinabeet <NyalSadiq@hotmail.com>
2016-09-26 18:06:04 +0100

    Add files via upload

---
+++ Web Program/confirmationpage.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>Confirmation of Order</title>
+		<link rel="stylesheet" type="text/css" href="StyleSheet.css"/>
+	</head>
+	<body>
+		<h1>Confirmation of Order</h1>
+		
+		<p> Thank you for shopping with us. </p>
+		<p> Below is a summary of your order. </p>
+	</body>
+</html>
+<?php	echo "Hello World"; 
+		echo 'First Name: '.$_POST["firstnames"].'<br>'; 
+		echo 'Family Name: '.$_POST["familyname"].'<br>' ; 
+		echo 'Email: '.$_POST["email"].'<br>' ;
+		echo 'Mobile Number: '.$_POST["mobilenumber"].'<br>'; 
+
+
+
+?>
+	
+
+		
+		
+
